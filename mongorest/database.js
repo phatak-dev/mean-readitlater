16206555c09ed4cbe01ecb5732cfc97d9d70b315
madhukar <phatak.dev@gmail.com>
2014-11-20 21:07:39 +0530

    fixed else bug

--- mongorest/database.js
+++ mongorest/database.js
@@ -11,8 +11,9 @@ function startFn(cb) {
   MongoClient.connect("mongodb://localhost:27017/test", 
   	function(err, db) {
     if(err)console.log(err);  		
+    else {
     module.exports.db = db;  
-    cb.call(this,[]);
+    cb.call(this,[]); }
   })}
 
 
